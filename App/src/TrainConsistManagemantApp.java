f7f3846bbb905a727123f3008b7bacd9c79981ed
varun99485-cell <varun99485@gmail.com>
2026-04-01 14:47:46 +0530

    Welcome to UC1

---
+++ App/src/TrainConsistManagemantApp.java
@@ -0,0 +1,11 @@
+public class TrainConsistManagemantApp {
+    public static void main(String[] args){
+
+        System.out.println("==================================");
+        System.out.println("=== Train Consist Management App ===");
+        System.out.println("==================================");
+
+        System.out.println("Train Initiated Successfully");
+    }
+
+}
