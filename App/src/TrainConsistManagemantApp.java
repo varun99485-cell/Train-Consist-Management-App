76e5bb2096ce70c774b393788c5ce392611b8931
varun99485-cell <varun99485@gmail.com>
2026-04-01 15:12:10 +0530

    Welcome to UC2

--- App/src/TrainConsistManagemantApp.java
+++ App/src/TrainConsistManagemantApp.java
@@ -1,11 +1,38 @@
+import java.util.ArrayList;
+
 public class TrainConsistManagemantApp {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        System.out.println("==================================");
-        System.out.println("=== Train Consist Management App ===");
-        System.out.println("==================================");
 
-        System.out.println("Train Initiated Successfully");
+        ArrayList<String> passengerBogies = new ArrayList<>();
+
+
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
+
+
+        System.out.println("Passenger Bogies after addition:");
+        System.out.println(passengerBogies);
+
+
+        passengerBogies.remove("AC Chair");
+
+
+        System.out.println("\nAfter removing AC Chair:");
+        System.out.println(passengerBogies);
+
+
+        boolean exists = passengerBogies.contains("Sleeper");
+
+        if (exists) {
+            System.out.println("\nSleeper bogie exists in the train.");
+        } else {
+            System.out.println("\nSleeper bogie does not exist.");
+        }
+
+
+        System.out.println("\nFinal Passenger Bogies:");
+        System.out.println(passengerBogies);
     }
-
 }
